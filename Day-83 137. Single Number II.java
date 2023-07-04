807b8472339952665da3def6ff26b9f24065f48f
yagyesh dubey <yagyesh2002.bpl@gmail.com>
2023-07-04 21:44:48 +0530

    Create Day-83 137. Single Number II.java

---
+++ Day-83 137. Single Number II.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+        
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        
+        return ones;
+    }
+}
