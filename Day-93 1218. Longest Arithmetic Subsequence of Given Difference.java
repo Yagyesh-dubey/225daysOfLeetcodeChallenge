eaa9f754f0a05c4395f8145251d42a9ad53b02e1
yagyesh dubey <yagyesh2002.bpl@gmail.com>
2023-07-14 05:58:54 +0530

    Create Day-93 1218. Longest Arithmetic Subsequence of Given Difference.java

---
+++ Day-93 1218. Longest Arithmetic Subsequence of Given Difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int longestSubsequence(int[] arr, int diff) {
+        int res = 1, n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i=0; i<n; i++){
+            int prev = arr[i]-diff, occ = map.getOrDefault(prev,0)+1;
+            map.put(arr[i], occ);
+            res = Math.max(res, occ);
+        }
+        return res;
+    }
+}
