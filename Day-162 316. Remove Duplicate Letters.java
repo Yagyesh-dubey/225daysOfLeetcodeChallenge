ef72cb8824d7baf19761c32cb7e9b9035067257d
yagyesh dubey <yagyesh2002.bpl@gmail.com>
2023-09-26 23:30:08 +0530

    Create Day-162 316. Remove Duplicate Letters.java

---
+++ Day-162 316. Remove Duplicate Letters.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public String removeDuplicateLetters(String s) {
+        Stack<Character> stack = new Stack<>();
+        Set<Character> seen = new HashSet<>();
+        Map<Character, Integer> lastOcc = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastOcc.put(s.charAt(i), i);
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!seen.contains(c)) {
+                while (!stack.isEmpty() && c < stack.peek() && i < lastOcc.get(stack.peek())) {
+                    seen.remove(stack.pop());
+                }
+                seen.add(c);
+                stack.push(c);
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+        return result.toString();
+    }
+}
