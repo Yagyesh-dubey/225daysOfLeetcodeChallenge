5c81d967879fc3ee116e6ae9669739b9d25a882e
yagyesh dubey <yagyesh2002.bpl@gmail.com>
2023-10-21 21:51:20 +0530

    Create Day-189 1425. Constrained Subsequence Sum.java

---
+++ Day-189 1425. Constrained Subsequence Sum.java
@@ -0,0 +1,33 @@
+import static java.lang.Integer.MIN_VALUE;
+import static java.lang.Math.max;
+
+import java.util.ArrayDeque;
+
+// coppied as I haven't studied D.P yet
+
+
+class Solution {
+    public int constrainedSubsetSum(int[] A, int k) {
+        var dq = new ArrayDeque<Data>();
+        int output = MIN_VALUE;
+
+        for (int i = 0; i < A.length; i++) {
+            if (!dq.isEmpty() && i - dq.peekFirst().i > k)
+                dq.pollFirst();
+
+            int x = max(0, dq.isEmpty() ? 0 : dq.peekFirst().x) + A[i];
+
+            output = max(output, x);
+
+            while (!dq.isEmpty() && dq.peekLast().x < x)
+                dq.pollLast();
+
+            dq.addLast(new Data(i, x));
+        }
+
+        return output;
+    }
+
+    private record Data(int i, int x) {}
+
+}
