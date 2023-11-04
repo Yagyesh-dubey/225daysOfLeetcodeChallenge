1faf365e27df334ff15f44b84c3038386620e69f
yagyesh dubey <yagyesh2002.bpl@gmail.com>
2023-11-04 23:58:23 +0530

    Create Day-206 1503. Last Moment Before All Ants Fall Out of a Plank.java

---
+++ Day-206 1503. Last Moment Before All Ants Fall Out of a Plank.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int res=0;
+        for(int l: left) {
+            res=Math.max(res, l);
+        }
+        for(int r: right) {
+            res=Math.max(res, n-r);
+        }
+
+        return res;
+    }
+}
